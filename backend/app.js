e4935a99886751305f4262cb5c3bf5319d007395
Anas Ahmad <anasahmad0239@gmail.com>
2025-10-18 13:20:50 +0530

    Initial setup

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+const app = express();
+
+logger.info('connecting to mongodb');
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to mongodb');
+	})
+	.catch((error) => {
+		logger.error('error connecting to mongodb', error.message);
+	});
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/', (req, res) => {
+	res.send('Hello world');
+});
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
